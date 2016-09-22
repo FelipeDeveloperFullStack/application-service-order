cd07f31987d46761f5c818846fd1cbd4df07e025
Felipe Miguel <felipe.miguel.santos@hotmail.com>
2016-09-22 18:38:46 -0300

    Commit

--- web-os/src/main/java/br/com/finance/financeiro_pessoal/ApplicationMain.java
+++ web-os/src/main/java/br/com/finance/financeiro_pessoal/ApplicationMain.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyMoneyApplication {
+public class ApplicationMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyMoneyApplication.class, args);
+		SpringApplication.run(ApplicationMain.class, args);
 	}
 }
