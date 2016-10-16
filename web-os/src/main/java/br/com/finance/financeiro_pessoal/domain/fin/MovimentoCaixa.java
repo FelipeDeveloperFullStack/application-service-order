3ee03e5774a75b0d10c87036aca0f2c175426b7f
Felipe <Felipe@192.168.1.4>
2016-10-16 02:31:46 -0200

    cc

--- web-os/src/main/java/br/com/finance/financeiro_pessoal/domain/fin/MovimentoCaixa.java
+++ web-os/src/main/java/br/com/finance/financeiro_pessoal/domain/fin/MovimentoCaixa.java
@@ -52,10 +52,6 @@ public class MovimentoCaixa extends GenericDomain{
 	@Enumerated(EnumType.STRING)
 	private TipoOrigemMovimento tipoOrigemMovimento;
 	
-	@OneToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "mc_saldo_financeiro")
-	private SaldoFinanceiro saldoFinanceiro;
-
 	public Parceiro getParceiro() {
 		return parceiro;
 	}
@@ -95,14 +91,5 @@ public class MovimentoCaixa extends GenericDomain{
 	public void setTipoOrigemMovimento(TipoOrigemMovimento tipoOrigemMovimento) {
 		this.tipoOrigemMovimento = tipoOrigemMovimento;
 	}
-
-	public SaldoFinanceiro getSaldoFinanceiro() {
-		return saldoFinanceiro;
-	}
-
-	public void setSaldoFinanceiro(SaldoFinanceiro saldoFinanceiro) {
-		this.saldoFinanceiro = saldoFinanceiro;
-	}
-	
 	
 }
