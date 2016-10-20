264aac06ac59b53aacbbada486e256de64164371
Felipe <Felipe@192.168.1.2>
2016-10-19 23:56:09 -0200

    ccc

--- web-os/src/main/java/br/com/finance/financeiro_pessoal/config/WebConfig.java
+++ web-os/src/main/java/br/com/finance/financeiro_pessoal/config/WebConfig.java
@@ -12,10 +12,11 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		ErrorPage error400 = new ErrorPage(HttpStatus.BAD_REQUEST, "/400");
 		ErrorPage error404 = new ErrorPage(HttpStatus.NOT_FOUND, "/404");
 		ErrorPage error500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500");
 		ErrorPage error403 = new ErrorPage(HttpStatus.FORBIDDEN, "/403");
-		return (container -> container.addErrorPages(error404, error403, error500));
+		return (container -> container.addErrorPages(error400, error404, error403, error500));
 	}
 	
 }
