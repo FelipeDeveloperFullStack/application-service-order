a4ea0c211aa50acc40237ac92c77657bb9daff2b
Felipe Miguel <felipe.miguel.santos@hotmail.com>
2016-10-20 13:26:18 -0200

    Commit - Implementação do modal de exclusão na página de movimento de caixa financeiro.

--- web-os/src/main/resources/static/dist/js/pages/application.js
+++ web-os/src/main/resources/static/dist/js/pages/application.js
@@ -7,6 +7,22 @@ function mascara_valores_decimais(){
 }
 //https://github.com/plentz/jquery-maskmoney
 
+// Página de movimento de caixa - exclusão
+$('#excluirMovimentoCaixaFinanceiro').on('show.bs.modal', function(event){
+	
+	var button = $(event.relatedTarget);
+	
+	var id = button.data('id');
+	var parceiro = button.data('parceiro');
+	var contaCaixa = button.data('conta');
+	var valorMovimento = button.data('valor');
+	
+	var modal = $(this);
+	modal.find('.modal-body span').html('Deseja realmente apagar o movimento de caixa financeiro? <p></p> Código: <strong>'
+			+id+'</strong> Parceiro: <strong>'+parceiro+'</strong> <p></p> Conta Caixa: <strong>'+contaCaixa+'</strong> Valor do movimento: <strong>'+valorMovimento+'</strong>');
+	
+});
+
 $(document).ready(function (){
 	mascara_valores_decimais();
 });	
